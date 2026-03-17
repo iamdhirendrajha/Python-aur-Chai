146ed3901a37851d136fa05380cd1158e19e64b0
iamlivelikedynamic <dhirendrakj714@gmail.com>
2026-03-17 21:08:41 +0530

    write few lines of code

---
+++ 01_Basics/Hello_Chai.py
@@ -0,0 +1,17 @@
+print("Chai aur Python, dono hi pyare hain!")
+
+def chai_kaise_banaye(n):
+    print("Chai banane ke liye, pehle paani garam karo.")
+    print("Phir chai patti daalo aur thoda sa doodh milao.")
+    print("Thodi der ke liye chhodo, phir chai ko chhano aur serve karo.")
+    print(f"Yeh chai {n} logon ke liye taiyaar hai!")
+    print(n)
+    
+chai_kaise_banaye(5)
+
+print("***********Another Simple Function***********")
+def chai(n):
+    print(n)
+
+chai(10)
+chai("Leameon Tea")
